e27b565bd0e1ac6567d58c042695f43f1301ac90
hossain <guljer.net@gmail.com>
2024-01-20 17:26:39 +0600

    add cv

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -14,14 +14,16 @@ function Banner() {
     <div className="bg-banner flex items-center justify-start lg:pl-14 pl-5">
       <div data-aos="fade-down">
         <h6 className="uppercase text-primary lg:text-[16px] text-[15px] font-medium tracking-widest">
-          Web Developer
+          Welcome To My World
         </h6>
         <h4 className="lg:w-[60%] h-full py-3 text-white leading-tight lg:text-[42px] text-[28px] font-bold">
-          My Name Is Guljer Hossain
+          Hi, I'm Md. Guljer Hossain
         </h4>
         <p className="text-[14px] font-light py-3 text-white lg:w-2/4 w-full">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
-          inventore quasi quod hic corporis quae, commodi debitis ea quam. Ea.
+          Architecting Extraordinary Web Experiences: Md Guljer Hossain -
+          Inspiring MERN Stack Developer with a Proven Track Record of Creating
+          Immersive User Journeys and Pushing the Boundaries of Digital
+          Innovation
         </p>
         <div className="flex items-center lg:space-x-5 my-4">
           <div className="lg:block hidden">
@@ -34,7 +36,7 @@ function Banner() {
           </div>
           <div>
             <a
-              href="CV.pdf"
+              href="GuljerCV.pdf"
               download
               className="flex lg:text-[15px] text-[14px] items-center lg:gap-3 gap-1 lg:px-5 px-3 py-2 rounded-md bg-primary text-white"
             >
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -31,15 +31,15 @@ function Main() {
           </div>
           <div className="flex items-center justify-center pt-[10px]">
             <div>
-              <h4 className="text-[26px] font-semibold">M. Guljer Hossain</h4>
+              <h4 className="text-[22px] font-semibold">M. Guljer Hossain</h4>
               <div className="flex items-center justify-center space-x-3 pt-2">
-                <span className="text-[20px] shadow-lg w-[30px] h-[30px] bg-slate-700 rounded-full flex items-center justify-center">
+                <span className="text-[18px] shadow-lg w-[30px] h-[30px] bg-slate-700 rounded-full flex items-center justify-center">
                   <FaGithub className="block" />
                 </span>
-                <span className="text-[20px] shadow-lg w-[30px] h-[30px] bg-slate-700 rounded-full flex items-center justify-center">
+                <span className="text-[18px] shadow-lg w-[30px] h-[30px] bg-slate-700 rounded-full flex items-center justify-center">
                   <FaLinkedinIn className="block" />
                 </span>
-                <span className="text-[20px] shadow-lg w-[30px] h-[30px] bg-slate-700 rounded-full flex items-center justify-center">
+                <span className="text-[18px] shadow-lg w-[30px] h-[30px] bg-slate-700 rounded-full flex items-center justify-center">
                   <FaFacebook className="block" />
                 </span>
               </div>
@@ -47,7 +47,7 @@ function Main() {
           </div>
           <div className="pt-[50px] pl-10">
             <ul className="space-y-5">
-              <li className="text-[16px] font-semibold">
+              <li className="text-[15px] font-light">
                 <NavLink
                   to="/"
                   className={({ isActive }) => (isActive ? "text-primary" : "")}
@@ -55,7 +55,7 @@ function Main() {
                   <IoMdHome className="inline-block mr-2 text-[24px]" /> Home
                 </NavLink>
               </li>
-              <li className="text-[16px] font-semibold">
+              <li className="text-[15px] font-light">
                 <NavLink
                   to="/about"
                   className={({ isActive }) => (isActive ? "text-primary" : "")}
@@ -63,7 +63,7 @@ function Main() {
                   <FcManager className="inline-block mr-2 text-[22px]" /> About
                 </NavLink>
               </li>
-              <li className="text-[16px] font-semibold">
+              <li className="text-[15px] font-light">
                 <NavLink
                   to="/service"
                   className={({ isActive }) => (isActive ? "text-primary" : "")}
@@ -72,7 +72,7 @@ function Main() {
                   Service
                 </NavLink>
               </li>
-              <li className="text-[16px] font-semibold">
+              <li className="text-[15px] font-light">
                 <NavLink
                   to="/portfolio"
                   className={({ isActive }) => (isActive ? "text-primary" : "")}
@@ -81,7 +81,7 @@ function Main() {
                   Portfolio
                 </NavLink>
               </li>
-              <li className="text-[16px] font-semibold">
+              <li className="text-[15px] font-light">
                 <NavLink
                   to="/contact"
                   className={({ isActive }) => (isActive ? "text-primary" : "")}
@@ -93,7 +93,7 @@ function Main() {
             </ul>
           </div>
           <div className="absolute bottom-3">
-            <p className="w-[70%] mx-auto text-left">
+            <p className="w-[70%] mx-auto text-left text-[14px] font-light">
               &copy; Copyright 2024 Designed By Guljer
             </p>
           </div>
@@ -115,36 +115,36 @@ function Main() {
         </div>
       </div>
       {menu && (
-        <div className="w-[300px] absolute top-0 left-0 bg-slate-900 h-screen z-50">
-          <div className="text-white">
+        <div className="w-[300px] absolute top-0 left-0 z-50">
+          <div className="text-white fixed w-[300px] h-screen bg-slate-900">
             <div className="mx-auto w-full mt-[20px]">
-              <div className="mx-auto w-[135px] shadow-lg rounded-full h-[135px] bg-gray-700 flex items-center justify-center">
+              <div className="mx-auto w-[150px] shadow-lg rounded-full h-[150px] bg-gray-700 flex items-center justify-center">
                 <img
                   src={Profile}
                   alt=""
-                  className="w-[120px] h-[120px] inline-block rounded-full"
+                  className="w-[135px] h-[135px] inline-block rounded-full"
                 />
               </div>
             </div>
             <div className="flex items-center justify-center pt-[10px]">
               <div>
-                <h4 className="text-[24px] font-semibold">M. Guljer Hossain</h4>
+                <h4 className="text-[22px] font-semibold">M. Guljer Hossain</h4>
                 <div className="flex items-center justify-center space-x-3 pt-2">
-                  <span className="text-[20px] shadow-lg w-[30px] h-[30px] bg-slate-700 rounded-full flex items-center justify-center">
+                  <span className="text-[18px] shadow-lg w-[30px] h-[30px] bg-slate-700 rounded-full flex items-center justify-center">
                     <FaGithub className="block" />
                   </span>
-                  <span className="text-[20px] shadow-lg w-[30px] h-[30px] bg-slate-700 rounded-full flex items-center justify-center">
+                  <span className="text-[18px] shadow-lg w-[30px] h-[30px] bg-slate-700 rounded-full flex items-center justify-center">
                     <FaLinkedinIn className="block" />
                   </span>
-                  <span className="text-[20px] shadow-lg w-[30px] h-[30px] bg-slate-700 rounded-full flex items-center justify-center">
+                  <span className="text-[18px] shadow-lg w-[30px] h-[30px] bg-slate-700 rounded-full flex items-center justify-center">
                     <FaFacebook className="block" />
                   </span>
                 </div>
               </div>
             </div>
-            <div className="pt-[40px] pl-14">
+            <div className="pt-[50px] pl-10">
               <ul className="space-y-5">
-                <li className="text-[16px] font-semibold">
+                <li className="text-[15px] font-light">
                   <NavLink
                     to="/"
                     className={({ isActive }) =>
@@ -154,18 +154,18 @@ function Main() {
                     <IoMdHome className="inline-block mr-2 text-[24px]" /> Home
                   </NavLink>
                 </li>
-                <li className="text-[16px] font-semibold">
+                <li className="text-[15px] font-light">
                   <NavLink
                     to="/about"
                     className={({ isActive }) =>
                       isActive ? "text-primary" : ""
                     }
                   >
-                    <FcManager className="inline-block mr-2 text-[22px]" />
+                    <FcManager className="inline-block mr-2 text-[22px]" />{" "}
                     About
                   </NavLink>
                 </li>
-                <li className="text-[16px] font-semibold">
+                <li className="text-[15px] font-light">
                   <NavLink
                     to="/service"
                     className={({ isActive }) =>
@@ -176,7 +176,7 @@ function Main() {
                     Service
                   </NavLink>
                 </li>
-                <li className="text-[16px] font-semibold">
+                <li className="text-[15px] font-light">
                   <NavLink
                     to="/portfolio"
                     className={({ isActive }) =>
@@ -187,7 +187,7 @@ function Main() {
                     Portfolio
                   </NavLink>
                 </li>
-                <li className="text-[16px] font-semibold">
+                <li className="text-[15px] font-light">
                   <NavLink
                     to="/contact"
                     className={({ isActive }) =>
@@ -201,7 +201,7 @@ function Main() {
               </ul>
             </div>
             <div className="absolute bottom-3">
-              <p className="w-[70%] mx-auto text-center">
+              <p className="w-[70%] mx-auto text-left text-[14px] font-light">
                 &copy; Copyright 2024 Designed By Guljer
               </p>
             </div>
